8528ea6db2e6444521167e73e089a6edaa4c47ce
Christopher Kramer <Hendrix1957@gmail.com>
2017-01-05 11:06:47 -0500

    my work

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,28 +9,33 @@ class Layout extends Component {
 
   render () {
     return <div className='layout'>
-      <h1>The Majestic That</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/menu'>Menu</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact Us</Link>
-          </li>
-          <li>
-            <Link to='/locations'>Locations</Link>
-          </li>
-        </ul>
-      </nav>
+      <h1>The Majestic Thai</h1>
+      <div className='border'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/menu'>Menu</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact Us</Link>
+            </li>
+            <li>
+              <Link to='/locations'>Locations</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {/* <div className='pics'>
+        <img src='https://4.bp.blogspot.com/-3FgGGBWeT8M/U8ITYqmAePI/AAAAAAABCrU/TNYq6fLr_Mg/s0/Urban+Park+Hotel+and+Spa%252C+Umhlanga+3.jpg' alt='4krestaraunt' width='100%' height='80%' />
+      </div> */}
       <main>
         {this.props.children}
       </main>
       <footer>
-        <p>&copy; 2017 The Majestic That.<br />Built with &hearts; at The Iron Yard, in St Petersburg, Florida.</p>
+        <p>&copy; 2017 The Majestic Thai.<br />Built with &hearts; at The Iron Yard, in St Petersburg, Florida.</p>
       </footer>
     </div>
   }
